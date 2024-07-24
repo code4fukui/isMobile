e7d0b75a5cc9548c92a948eb2c25345c691762ef
Taisuke Fukuno <fukuno@jig.jp>
2024-07-24 09:45:57 +0900

    mv

---
+++ isMobile.test.js
@@ -0,0 +1,8 @@
+import * as t from "https://deno.land/std/testing/asserts.ts";
+import isMobile from "./isMobile.js";
+
+Deno.test("simple", () => {
+  const userAgent = 'Mozilla/5.0 (Linux; U; Android android-version; locale; KFOT Build/product-build) AppleWebKit/webkit-version (KHTML, like Gecko) Silk/browser-version like Chrome/chrome-version Safari/webkit-version';
+  const mobile = isMobile(userAgent);
+  t.assert(mobile);
+});
